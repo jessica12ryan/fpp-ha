8b2d878aab82b414725347fb2ba76183a604b2ce
jessica12ryan <jessica12ryan@gmail.com>
2026-06-27 20:12:08 -0300

    --

--- fpp-ha-stats/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bash
-# Manually launch both processes to avoid s6-overlay service directory conflicts
-cd /app/server
-node server.js &
-cd /app/statsCollector
-node collector.js &
-wait -n
